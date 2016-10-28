86d5a986959c9532f9e4ac224ab1c174cc14ac1d
Jacob Knaack <jacobknck@gmail.com>
2016-10-28 13:52:06 -0700

    added log-model with router and controller, tested all routes

--- route/auth-router.js
+++ route/auth-router.js
@@ -2,7 +2,7 @@
 
 // npm modules
 const Router = require('express').Router;
-const debug = require('debug')('authdemo:auth-router');
+const debug = require('debug')('appMileage:auth-router');
 const jsonParser = require('body-parser').json();
 const parseBasicAuth = require('../lib/parse-basic-auth');
 
@@ -13,12 +13,14 @@ const authController = require('../controller/auth-controller');
 const authRouter = module.exports = new Router();
 
 authRouter.post('/signup', jsonParser, function(req, res, next){
+  debug('user signup');
   authController.signup(req.body)
   .then( token => res.send(token))
   .catch(next);
 });
 
 authRouter.get('/signin', parseBasicAuth ,function(req, res, next){
+  debug('user signin');
   console.log('req.auth', req.auth);
   authController.signin(req.auth)
   .then( token => res.send(token))
